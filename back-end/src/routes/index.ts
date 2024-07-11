d4ef08cd2281094f7a8e8adc7bbbae099cebe4cb
Ahmad Raza <ahmadraza77887087@gmail.com>
2024-07-11 20:44:05 +0500

    Shifted backend to JavaScript

--- back-end/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getUsers } from '../controllers/auth_controller';
-
-const router: Router = Router();
-
-router.get('/', getUsers);
-
-export default router;
