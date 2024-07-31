93a3f15f32a18a5d7c94a91dd0e63ad65ab4fe0b
haniyatj <haniya19992020@gmail.com>
2024-07-31 11:26:34 +0500

    eeee

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -94,7 +94,7 @@ const forgotPassword = async (req, res) => {
             html: `
                 <p>You are receiving this because you (or someone else) have requested the reset of the password for your account.</p>
                 <p>Please click on the following link, or paste this into your browser to complete the process:</p>
-                <p><a href="http://${req.headers.host}/reset/${token}">Reset Password</a></p>
+                <p><a href="http://localhost:3000/reset-password/?token=${token}">Reset Password</a></p>
                 <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
             `
         };
