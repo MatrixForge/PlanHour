a8d7e5ebfada6ddc5588f48ea54772afaf16165c
yahyachughtai <107436277+yahyachughtai@users.noreply.github.com>
2024-08-02 16:17:43 +0500

    fix: sort icon on eventify page updated

--- front-end/app/components/secondNavbg.tsx
+++ front-end/app/components/secondNavbg.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import SearchBar from '../components/searchBar'; // Adjust the import path and props type as per your actual SearchBar component
+import React from "react";
+import SearchBar from "../components/searchBar"; // Adjust the import path and props type as per your actual SearchBar component
 
 const Navbar2: React.FC = () => {
   return (
-    <nav style={{
-      backgroundImage: 'url("/navbg.png")',
-      backgroundSize: 'cover',
-      minHeight: '120px',
-      display: 'flex',
-      justifyContent: 'center',
-    }}>
-      <SearchBar  />
+    <nav
+      style={{
+        backgroundImage: 'url("/navbg.png")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        minHeight: "120px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <SearchBar />
     </nav>
   );
 };
